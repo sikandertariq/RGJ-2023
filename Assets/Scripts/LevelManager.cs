9d57942088abea36eff55ced0201cb24eff1d362
Rimsha <rimshaansar07@gmail.com>
2023-08-26 17:17:22 +0500

    Ui added, code refactored

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,16 +6,33 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] tinies;
-
+    [SerializeField] private GameObject pauseBtn;
+    [SerializeField] private GameObject pauseMenu;
 
     void Update()
     {
         tinies = GameObject.FindGameObjectsWithTag("Tiny");
         if (tinies.Length == 0)
-            if (SceneManager.GetActiveScene().buildIndex == 5)  // == 6
+            if (SceneManager.GetActiveScene().name == "5")  // == 6
                 SceneManager.LoadScene(0);
             else
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
+    }
+    public void OnClick_PauseBtn()
+    {
+        Time.timeScale = 0;
+        pauseBtn.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+    public void OnClick_Resume()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        pauseBtn.SetActive(true);
+    }
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
